f24e4a1a9c50966d60aaf9058296fc46df2ab77b
Jay McBee <jmcbee1@gmail.com>
2016-05-30 16:48:26 -0500

    palindrome

---
+++ palindromeForYourDome.js
@@ -0,0 +1,35 @@
+// Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+// Like the above examples, here are a few test cases which are also populated:
+
+// "Amore, Roma" => valid
+// "A man, a plan, a canal: Panama" => valid
+// "No 'x' in 'Nixon'" => valid
+// "Abba Zabba, you're my only friend" => invalid
+
+// You can see that they are case insensitive and disregards non alphanumeric characters. 
+// In addition to a few predefined tests, your function will also be tested against 
+// a random string generator 50 times which are guaranteed to produce valid palindromes.
+
+// Notes:
+
+// reverse/reverse! have been disabled for String/Array and reverse() for JS.
+// reverse cannot get used in Haskell either
+// the empty string "" can be read forward or backward the same, it's a palindrome in our case.
+
+
+//solution
+
+let palindrome = (string) => {
+ 
+  let cleanString = string
+    .replace(/\s|\W/g,"")
+    .toLowerCase();
+ 
+  for(let i = 0; i < Math.floor(cleanString.length/2); i++){
+    if(cleanString[i] !== cleanString[cleanString.length - i - 1]){
+      return false;
+    }
+  }
+  return true;
+}
