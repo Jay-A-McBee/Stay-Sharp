54cb91464ca0cc6446e15f6b49220acd8f6c2d32
Jay McBee <jmcbee1@gmail.com>
2016-06-19 10:34:58 -0500

    sortedList

---
+++ sortedListClass.js
@@ -0,0 +1,44 @@
+
+
+// The task here is to create a class that maintains a sorted list of numbers in ascending order.
+
+// The class will have two methods:
+
+// add(x) will add x to the list
+// get(i) will get the ith value in the list
+// You should also provide a length property that gives the length of the list.
+
+// Efficiency matters!
+
+
+class SortedList {
+  
+  constructor(){
+    this.length = 0,
+    this.list = []
+  }
+  
+  add(val){
+    
+    let middleIdx = Math.floor(this.length/2);
+    this.length++;
+    
+    let { i , len }  = val < this.list[middleIdx] ? 
+      {i : 0, len: middleIdx} : 
+      {i: middleIdx + 1, len: this.length};
+ 
+    for( i; i < this.length; i++){
+      if(this.list[i] >= val){
+        this.list.splice(i,0,val);
+        return;
+      }
+  
+    }
+  this.list.push(val);
+  }
+
+  get(i){
+    return this.list[i];
+  }
+
+}
